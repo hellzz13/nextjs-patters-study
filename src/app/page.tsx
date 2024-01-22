6c2d0cfc05ca3efe5445e303cef0ae4b255a02a9
Hélio Neto <hpalenca13@gmail.com>
2024-01-22 00:06:49 -0300

    ajustments config

---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+import List from "@/src/client/components/List";
+import React from "react";
+
+export default function Home() {
+  return <List />;
+}
---
+++ src/client/components/List/index.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import useSWR from "swr";
+import Loading from "../Loading";
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+const api = "https://rickandmortyapi.com/api/character";
+
+export default function List() {
+  const { data, error, isLoading } = useSWR(api, fetcher);
+
+  if (error) return "An error has occurred.";
+  if (isLoading) return <Loading />;
+
+  console.log(data);
+  return (
+    <div className="text-white">
+      <ul id="data">
+        {data.results.map((item: any, key: string) => (
+          <li key={key}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
